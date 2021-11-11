502eefdd2bd5d584bc913e002db85aa1afa2a33a
legendateam <undertaker176@gmail.com>
2021-11-11 14:24:14 +0200

    classwork - 1

---
+++ lesson1/classwork/main.js
@@ -0,0 +1,53 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let number = [40, 8, 287, 15, 29, 101, 1123, 58, 3, 77];
+// let result = number[0] + number[1] + number[2] + number[3] + number[4] + number[5] + number[6] + number[7] + number [8] + number[9];
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//     name: "harry potter",
+//     number: 570,
+//     genre: "fantastic"
+// }
+// console.log(book);
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let book = {
+//     name: "zakhar Berkut",
+//     number: 10,
+//     genre: "fantastic",
+//     athors: {
+//         name: "Ivan Franko",
+//         age: 41
+//     }
+// }
+// console.log(book);
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let book = [
+//     {name: "harry potter", number: 570, genre: "fantastic", athors: {name:"Joanne Rowling", age: 15}},
+//     {name: "zakhar Berkut", number: 10, genre: "fantastic", athors: {name: "Ivan Franko", age: 41}}
+// ]
+// console.log(book);
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s
+// let height = 23;
+// let width = 10;
+// let s = .5*height*width;
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// let heightC = 10;
+// let dC = 4;
+// let v = (3.14 * dC**2) * heightC;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
+// let n = 3;
+// let m  = 4;
+// let k = Math.pow(n,2) + Math.pow(m,2);
+// console.log(k);
