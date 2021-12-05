69cbec83685ac2ff885dbc0b13da4cdc69501c87
legendateam <undertaker176@gmail.com>
2021-12-05 23:40:10 +0200

    additional - 8

--- lesson8/additional/main.js
+++ lesson8/additional/main.js
@@ -1,1 +1,18 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
+
+let arr = []
+function recCall(classlist) {
+    if(classlist.children.length) {
+        for (const element of classlist.children) {
+            if(element.classList) {
+                for (const e of element.classList) {
+                   arr.push(e);
+                }
+            }
+            recCall(element)
+        }
+    }
+}
+
+recCall(document.body);
+console.log(arr);
