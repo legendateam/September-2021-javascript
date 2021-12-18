e0592c1be4f342584e1123e5e155b1975a8b41fb
legendateam <undertaker176@gmail.com>
2021-12-18 22:53:25 +0200

    class work - 12

--- lesson12/class work/main.js
+++ lesson12/class work/main.js
@@ -2,3 +2,28 @@
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(value => {
+        value.forEach(post => {
+            let div = document.createElement("div");
+            let btn = document.createElement("button");
+            btn.innerText = "comments post";
+            div.innerText = `${post.id} ${post.title} `;
+            div.appendChild(btn);
+            document.body.appendChild(div);
+
+            btn.onclick = () => {
+                let commentsDiv = document.createElement("div");
+                if(div.children.length === 1) {
+                    commentsDiv.innerText = `${post.body}`;
+                    div.appendChild(commentsDiv);
+                } else {
+                   let child = div.getElementsByTagName("div")[0];
+                    div.removeChild(child)
+                }
+            }
+        })
+    })
