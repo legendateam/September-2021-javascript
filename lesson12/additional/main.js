b4bc606ea4a3fd6fe3d6594ab6a5b62ebf0d29bc
legendateam <undertaker176@gmail.com>
2021-12-20 16:12:30 +0200

    additional fix - 12

--- lesson12/additional/main.js
+++ lesson12/additional/main.js
@@ -4,69 +4,64 @@
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
 
-let mainElement = document.createElement("div");
-mainElement.setAttribute("class","wrap")
-document.body.appendChild(mainElement);
+let usersClassElements = document.getElementsByClassName("users")[0];
 
-let wrapClass = document.getElementsByClassName("wrap")[0];
 
-let itarElements = (elements) => {
-    for (const e of elements) {
-        return e;
-    }
-}
 
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => {
-        users.forEach(user => {
+    .then(responseUsers => responseUsers.json())
+    .then(usersArr => {
+        usersArr.forEach(user => {
             let userDiv = document.createElement("div");
-            userDiv.setAttribute("class", "user");
             let userBtn = document.createElement("button");
-            userBtn.innerText = "post";
-            userDiv.innerText = `${user.name} `;
+            userDiv.classList.add("user");
+            userDiv.innerText = `${user.id}.  ${user.name} `;
+            userBtn.innerText = "show posts";
             userDiv.appendChild(userBtn);
-            wrapClass.appendChild(userDiv);
+            usersClassElements.appendChild(userDiv);
+
             userBtn.onclick = () => {
-                let postClass = document.getElementsByClassName("post");
-                if(typeof itarElements(postClass) === "undefined") {
-                    fetch("https://jsonplaceholder.typicode.com/posts")
-                        .then(resp => resp.json())
+                if(userDiv.children.length < 2) {
+                    fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+                        .then(responsePosts => responsePosts.json())
                         .then(posts => {
+                            let postsDiv = document.createElement("div");
+                            postsDiv.classList.add("posts");
+                            userDiv.appendChild(postsDiv);
                             posts.forEach(post => {
-                                if(user.id === post.userId) {
-                                    let postDiv = document.createElement("div");
-                                    postDiv.setAttribute("class", "post");
-                                    let postBtn = document.createElement("button");
-                                    postDiv.innerText = `${post.title} `;
-                                    postBtn.innerText = "comments";
-                                    postDiv.appendChild(postBtn);
-                                    userDiv.appendChild(postDiv);
-                                    postBtn.onclick = () => {
-                                        let commentClass = document.getElementsByClassName("comment");
-                                        if (typeof itarElements(commentClass) === "undefined") {
-                                            fetch("https://jsonplaceholder.typicode.com/comments")
-                                                .then(someResponse => someResponse.json())
-                                                .then(comments => {
-                                                    comments.forEach(comment => {
-                                                        if (post.id === comment.postId) {
-                                                            let commentDiv = document.createElement("div");
-                                                            commentDiv.setAttribute("class", "comment");
-                                                            commentDiv.innerText = `${comment.body}`;
-                                                            postDiv.appendChild(commentDiv);
-                                                        }
-                                                    })
+                                let postDiv = document.createElement("div");
+                                postDiv.classList.add("post")
+                                let postBtn = document.createElement("button");
+                                postBtn.innerText = "show comments";
+                                postDiv.innerText = `${post.userId}. ${post.id} - ${post.title} `;
+                                postDiv.appendChild(postBtn);
+                                postsDiv.appendChild(postDiv);
+
+                                postBtn.onclick = () => {
+                                    if(postDiv.children.length < 2 ) {
+                                        fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+                                            .then(responseComments => responseComments.json())
+                                            .then(comments => {
+                                                let commentsDiv = document.createElement("div");
+                                                commentsDiv.classList.add("comments");
+                                                postDiv.appendChild(commentsDiv);
+                                                comments.forEach(comment => {
+                                                    let commentDiv = document.createElement("div");
+                                                    commentDiv.classList.add("comment");
+                                                    commentDiv.innerText = `${comment.postId}.${comment.id} : ${comment.name}  ${comment.body}`;
+                                                    commentsDiv.appendChild(commentDiv);
                                                 })
-                                        } else {
-                                            itarElements(postClass).removeChild(itarElements(commentClass));
-                                        }
+                                            })
+                                    } else {
+                                        let commentsClass = document.getElementsByClassName("comments");
+                                        postDiv.removeChild(commentsClass[0])
                                     }
                                 }
                             })
                         })
                 } else {
-                    let userClass = document.getElementsByClassName("user");
-                    itarElements(userClass).removeChild(itarElements(postClass));
+                    let postsClass = document.getElementsByClassName("posts");
+                    userDiv.removeChild(postsClass[0]);
                 }
             }
         })
